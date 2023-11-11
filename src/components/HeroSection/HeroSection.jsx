a46581b870f773a23749adee3eda1ffe0173d4a0
shafin90 <mashrafiahnam1@gmail.com>
2023-11-10 22:29:55 -0800

    responsive issue

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -21,7 +21,7 @@ const HeroSection = ({heading, article, btn, fontweightOfArticle}) => {
                 </article>
 
                 {/* hero-section-button-container */}
-                <div className={!btn?" d-none ":" d-flex  justify-content-start  align-content-center "}>
+                <div className={!btn?" d-none ":" d-flex  justify-content-start  align-content-center w-100 "}>
                     <button className="get-started">Get Started</button>
                     <button className="watch-video">
                         <img className="play-icon" src="/assets/Images/play-icon.png" alt="" />
