b988eca5d98792dcda295ada09357c58842ed6ce
shafin90 <mashrafiahnam1@gmail.com>
2023-11-10 20:37:31 -0800

    push

--- src/components/BackgroundImageContainer/BackgroundImageContainer.jsx
+++ src/components/BackgroundImageContainer/BackgroundImageContainer.jsx
@@ -4,7 +4,7 @@ import './BackgroundImageContainer.css'
 const BackgroundImageContainer = ({rightSideImg}) => {
     return (
         <div style={{zIndex:"0"}} className=" d-flex  justify-content-between  align-content-center position-absolute top-0  w-100 ">
-            <img className="left-side-image-background" src="../../../public/assets/Images/left-img-of-banner.png" alt="" />
+            <img className="left-side-image-background" src="/public/assets/Images/left-img-of-banner.png" alt="" />
             <img className="right-side-image-background" src={rightSideImg} alt="" />
         </div>
     );
