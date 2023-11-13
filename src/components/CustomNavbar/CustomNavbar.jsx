a322c1c6703a23670da75f57d7a1f29b61669720
shafin90 <mashrafiahnam1@gmail.com>
2023-11-13 00:05:54 -0800

    working with proper documentation

--- src/components/CustomNavbar/CustomNavbar.jsx
+++ src/components/CustomNavbar/CustomNavbar.jsx
@@ -9,9 +9,7 @@ import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
 import "./CustomNavbar.css"
 import { Link } from "react-router-dom";
 
-
 const CustomNavbar = () => {
-
     return (
         <Navbar expand="lg" className="py-0 w-100 ">
             <Container fluid style={{ width: "88.88888888888889vw" }} className=" d-flex  justify-content-between  align-content-center px-0 mx-0  mx-auto marginTopOfCustomNavBar" >
@@ -25,20 +23,21 @@ const CustomNavbar = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto d-flex  justify-content-between  align-items-center nav-menu-container">
+
                         <Link className="nav-menu text-decoration-none" to="/">Home</Link>
-                        {/* <Link className="nav-menu text-decoration-none" to="#link"></Link> */}
-                       
+
+                        {/*Dropdown menu---------------  */}
                         <NavDropdown className="nav-menu" title="Products" id="basic-nav-dropdown" >
                             <NavDropdown.Item as={Link} to="/productsPharma">Pharma</NavDropdown.Item>
                             <NavDropdown.Item as={Link} to="/productsNutra">Nutra </NavDropdown.Item>
                             <NavDropdown.Item as={Link} to="/productsVetarn">Vetarn</NavDropdown.Item>
-                            {/* <NavDropdown.Divider /> */}
                             <NavDropdown.Item as={Link} to="/productsHerbal"> Herbal </NavDropdown.Item>
                             <NavDropdown.Item as={Link} to="/productsCosmetics"> Cosmetics </NavDropdown.Item>
                             <NavDropdown.Item as={Link} to="/productsFragrance"> Fragrance </NavDropdown.Item>
                             <NavDropdown.Item as={Link} to="/productsSexual"> Sexual </NavDropdown.Item>
                         </NavDropdown>
-                       
+                        {/*Dropdown menu---------------  */}
+
                         <Link className="nav-menu text-decoration-none" to="#link">Services</Link>
                         <Link className="nav-menu text-decoration-none" to="#link">Exports</Link>
                         <Link className="nav-menu text-decoration-none " to="/aboutUs">About Us</Link>
--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,28 +1,42 @@
-// Component Explaination: Hero section of this website. It takes place after CustomNavbar component.
-// CSS class explaination
+// Component Explaination==================================================== 
+
+/* Hero section of this website. It takes place after CustomNavbar component.
+ It takes 4 props===
+ 1. heading: Large size heading text of this section
+ 2. article: an article after the heading
+ 3. btn: btn is either true or false. If btn====true, then button section will be visible otherwise button section will be display none. Because in most case, there is no button in Hero section. 
+ 4. fontweightOfArticle: font-size of article. 
+ */
 
 import { Container } from "react-bootstrap";
 import './HeroSection.css'
 
-
-const HeroSection = ({heading, article, btn, fontweightOfArticle}) => {
+const HeroSection = ({ heading, article, btn, fontweightOfArticle }) => {
     return (
+
+        /* hero-section: this is outer container of the hero section. it contains. inside-container-herosection . */
         <Container fluid className="hero-section d-flex  justify-content-lg-start  align-content-center px-0  ">
+
+            {/* inside-container-herosection: It is the inner container of hero-section. It contains the heading, article and button. */}
             <div className="inside-container-herosection">
 
-                {/*===== Hero section heading =======*/}
+                {/*hero-section-heading: heading of this component.*/}
                 <h1 className="hero-section-heading">
                     {heading}
                 </h1>
 
-                {/* hero-section-article */}
-                <article style={{fontWeight:fontweightOfArticle}} className="hero-section-article">
+                {/* hero-sectiuon-article: article of this component. */}
+                <article style={{ fontWeight: fontweightOfArticle }} className="hero-section-article">
                     {article}
                 </article>
 
-                {/* hero-section-button-container */}
-                <div className={!btn?" d-none ":" d-flex  justify-content-start  align-content-center w-100 "}>
+                {/* hero-section-button-container: It contains the buttons of this component. In some page's there will not be any button. So, if btn props is true, only then it will be visible */}
+                <div className={!btn ? " d-none " : " d-flex  justify-content-start  align-content-center w-100 "}>
+                    
+                    {/* Get started button */}
                     <button className="get-started">Get Started</button>
+                    
+                    {/*  Watch vide button */}
                     <button className="watch-video">
                         <img className="play-icon" src="/assets/Images/Essential icon.png" alt="" />
                         Watch video
