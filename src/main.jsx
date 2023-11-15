8f3ccdf1ed81b129ae13f6610e84f3698494fbc2
shafin90 <mashrafiahnam1@gmail.com>
2023-11-14 17:25:18 -0800

    push

--- src/main.jsx
+++ src/main.jsx
@@ -6,18 +6,18 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
-import Home from './components/Home/Home';
-import AboutUs from './components/AboutUs/AboutUs';
-import ContactUs from './components/ContactUs/ContactUs';
-import OurTeam from './components/OurTeam/OurTeam';
-import Products_Pharma from './components/Products_Pharma/Products_Pharma';
-import Products_Nutra from './components/Products_Nutra/Products_Nutra';
-import Products_vetarn from './components/Products_vetarn/Products_vetarn';
-import Products_herbal from './components/Products_Herbal/Products_herbal';
-import Products_Cosmetics from './components/Products_Cosmetics/Products_Cosmetics';
-import Products_Fragrance from './components/Products_Fragrance/Products_Fragrance';
-import Products_Sexual from './components/Products_Sexual/Products_Sexual';
-import NotFound from './components/NotFound/NotFound';
+import Home from './pages/Home/Home';
+import AboutUs from './pages/AboutUs/AboutUs';
+import ContactUs from './pages/ContactUs/ContactUs';
+import OurTeam from './pages/OurTeam/OurTeam';
+import Products_Pharma from './pages/Products_Pharma/Products_Pharma';
+import Products_Nutra from './pages/Products_Nutra/Products_Nutra';
+import Products_vetarn from './pages/Products_vetarn/Products_vetarn';
+import Products_herbal from './pages/Products_Herbal/Products_herbal';
+import Products_Cosmetics from './pages/Products_Cosmetics/Products_Cosmetics';
+import Products_Fragrance from './pages/Products_Fragrance/Products_Fragrance';
+import Products_Sexual from './pages/Products_Sexual/Products_Sexual';
+import NotFound from './pages/NotFound/NotFound';
 
 
 const router = createBrowserRouter([
