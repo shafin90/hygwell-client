20c45625928bb61a620a0efd31eedd33e5903d70
shafin90 <mashrafiahnam1@gmail.com>
2023-11-15 19:05:17 -0800

    missing image problem in export page is solved

--- src/components/BoxContentContainer_ExportPage/BoxContentContainer_ExportPage.jsx
+++ src/components/BoxContentContainer_ExportPage/BoxContentContainer_ExportPage.jsx
@@ -5,12 +5,12 @@ const BoxContentContainer_ExportPage = () => {
     return (
         <div className='boxContentContainer_ExportPage my-5  d-flex flex-column flex-md-row  flex-lg-row  justify-content-between  align-items-center '>
             <BoxContent_ExportsPage
-            img='../../../public/assets/Images/image 1.png'
+            img='/assets/Images/image 1.png'
             heading="Reegulatory Compliance"
             ></BoxContent_ExportsPage>
             
             <BoxContent_ExportsPage
-            img='../../../public/assets/Images/image 4.png'
+            img='/assets/Images/image 4.png'
             heading="Wide product range"
             ></BoxContent_ExportsPage>
         </div>
