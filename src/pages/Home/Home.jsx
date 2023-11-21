dd5a6a262d4e3cddee7738f2e8315413365a7473
shafin90 <mashrafiahnam1@gmail.com>
2023-11-20 18:19:41 -0800

    push

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,45 +1,75 @@
+import React, { Suspense } from "react";
 import { Container } from "react-bootstrap";
-import Banner from "../../components/Banner/Banner";
-import BackgroundImageContainer from "../../components/BackgroundImageContainer/BackgroundImageContainer";
-import AboutUs_HomePage from "../../components/AboutUs_HomePage/AboutUs_HomePage";
-import OurOfferings_HomePage from "../../components/OurOfferings_HomePage/OurOfferings_HomePage";
-import OurValues_HomePage from "../../components/OurValues_HomePage/OurValues_HomePage";
-import ProductShowCase_HomePage from "../../components/ProductShowCase_HomePage/ProductShowCase_HomePage";
-import OurVision from "../../components/OurVision/OurVision";
-import TechniqalHealthCareSection_HomPage from "../../components/TechniqalHealthCareSection_HomPage/TechniqalHealthCareSection_HomPage";
-import MeetOurTeam from "../../components/MeetOurTeam/MeetOurTeam";
-import GetYourPharmacy_HomePage from "../../components/GetYourPharmacy_HomePage/GetYourPharmacy_HomePage";
-import BeforeFooter_HomePage from "../../components/BeforeFooter_HomePage/BeforeFooter_HomePage";
-import Footer from "../../components/Footer/Footer";
 
-
+const LazyBanner = React.lazy(() => import("../../components/Banner/Banner"));
+const LazyBackgroundImageContainer = React.lazy(() => import("../../components/BackgroundImageContainer/BackgroundImageContainer"));
+const LazyAboutUs = React.lazy(() => import("../../components/AboutUs_HomePage/AboutUs_HomePage"));
+const LazyOurOfferings = React.lazy(() => import("../../components/OurOfferings_HomePage/OurOfferings_HomePage"));
+const LazyOurValues = React.lazy(() => import("../../components/OurValues_HomePage/OurValues_HomePage"));
+const LazyProductShowCase = React.lazy(() => import("../../components/ProductShowCase_HomePage/ProductShowCase_HomePage"));
+const LazyOurVision = React.lazy(() => import("../../components/OurVision/OurVision"));
+const LazyTechnicalHealthCareSection = React.lazy(() => import("../../components/TechniqalHealthCareSection_HomPage/TechniqalHealthCareSection_HomPage"));
+const LazyMeetOurTeam = React.lazy(() => import("../../components/MeetOurTeam/MeetOurTeam"));
+const LazyGetYourPharmacy = React.lazy(() => import("../../components/GetYourPharmacy_HomePage/GetYourPharmacy_HomePage"));
+const LazyBeforeFooter = React.lazy(() => import("../../components/BeforeFooter_HomePage/BeforeFooter_HomePage"));
+const LazyFooter = React.lazy(() => import("../../components/Footer/Footer"));
 
 const Home = () => {
-   
-
     return (
-        <Container fluid className=" px-0  overflow-x-hidden overflow-y-hidden ">
-         
-            <Banner
-                heading="Welcome To HYGWELL"
-                article="Your Trusted Partner in Healthcare Solutions"
-                btn={true}
-                fontweightOfArticle="500"
-            ></Banner>
-            <BackgroundImageContainer
-                rightSideImg="/assets/Images/Group 1171282607.png"
-            ></BackgroundImageContainer>
-            <AboutUs_HomePage></AboutUs_HomePage>
-            <OurOfferings_HomePage></OurOfferings_HomePage>
-            <OurValues_HomePage></OurValues_HomePage>
-            <ProductShowCase_HomePage></ProductShowCase_HomePage>
-            <OurVision></OurVision>
-            <TechniqalHealthCareSection_HomPage></TechniqalHealthCareSection_HomPage>
-            <MeetOurTeam></MeetOurTeam>
-            <GetYourPharmacy_HomePage></GetYourPharmacy_HomePage>
-            <BeforeFooter_HomePage></BeforeFooter_HomePage>
-            <Footer></Footer>
-        </Container>
+        <Container fluid className="px-0 overflow-x-hidden overflow-y-hidden">
+            <Suspense fallback={<div>Loading...</div>}>
+                <LazyBanner
+                    heading="Welcome To HYGWELL"
+                    article="Your Trusted Partner in Healthcare Solutions"
+                    btn={true}
+                    fontweightOfArticle="500"
+                />
+            </Suspense>
+
+            <Suspense>
+                <LazyBackgroundImageContainer rightSideImg="/assets/Images/Group 1171282607.png" />
+            </Suspense>
+
+            <Suspense>
+                <LazyAboutUs />
+            </Suspense>
+
+            <Suspense>
+                <LazyOurOfferings />
+            </Suspense>
+
+            <Suspense>
+                <LazyOurValues />
+            </Suspense>
+
+            <Suspense>
+                <LazyProductShowCase />
+            </Suspense>
+
+            <Suspense>
+                <LazyOurVision />
+            </Suspense>
+
+            <Suspense>
+                <LazyTechnicalHealthCareSection />
+            </Suspense>
+
+            <Suspense>
+                <LazyMeetOurTeam />
+            </Suspense>
+
+            <Suspense>
+                <LazyGetYourPharmacy />
+            </Suspense>
+
+            <Suspense>
+                <LazyBeforeFooter />
+            </Suspense>
+
+            <Suspense>
+                <LazyFooter />
+            </Suspense>
+        </Container >
     );
 };
 
