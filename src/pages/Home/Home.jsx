c8a5da7e03cdca5f3b9137e95dda18c708c83b93
shafin90 <mashrafiahnam1@gmail.com>
2023-12-25 04:20:02 -0800

    push

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from "react";
 import { Container } from "react-bootstrap";
+import './Home.css'
 
 const LazyBanner = React.lazy(() => import("../../components/Banner/Banner"));
 const LazyBackgroundImageContainer = React.lazy(() => import("../../components/BackgroundImageContainer/BackgroundImageContainer"));
@@ -13,10 +14,16 @@ const LazyMeetOurTeam = React.lazy(() => import("../../components/MeetOurTeam/Me
 const LazyGetYourPharmacy = React.lazy(() => import("../../components/GetYourPharmacy_HomePage/GetYourPharmacy_HomePage"));
 const LazyBeforeFooter = React.lazy(() => import("../../components/BeforeFooter_HomePage/BeforeFooter_HomePage"));
 const LazyFooter = React.lazy(() => import("../../components/Footer/Footer"));
+import AnimatedCursor from "react-animated-cursor"
+
 
 const Home = () => {
     return (
-        <Container fluid className="px-0 overflow-x-hidden overflow-y-hidden">
+        <Container fluid className="px-0 overflow-x-hidden overflow-y-hidden home">
+             <AnimatedCursor 
+             
+             color='25, 135, 84'
+             />
             <Suspense fallback={<div>Loading...</div>}>
                 <LazyBanner
                     heading="Welcome To HYGWELL"
